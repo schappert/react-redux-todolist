fd9a3ec5f813bdd861096673a5531c29ce8a0728
sebastien_chapert <sebastien.chappert@hotmail.fr>
2025-09-13 16:27:58 +0200

    Correct define config for Vite test

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-
+/// <reference types="vitest" />
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
   plugins: [react()],
   test: {
     globals: true,
-    environment: "jsdom", // simulation du navigateur
-    setupFiles: "./src/setupTests.ts", // fichier pour setup (jest-dom, etc.)
+    environment: "jsdom",
+    setupFiles: "./src/setupTests.ts",
   },
-})
+});
+
